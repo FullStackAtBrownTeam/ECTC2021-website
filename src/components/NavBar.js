e2aa9a9eb58c71c91f263920daa4db7328eea55b
Eva Lau <eva_lau@brown.edu>
2020-05-19 22:11:08 -0400

    add logo and text, stickied at top, transparent

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap';
-import './NavBar.css'
 
 export class Navigation extends Component {
     render() {
         return (
-            <Navbar className="navigation">
-                <Navbar.Brand href="#home">
+            <Navbar className="navigation" sticky="top">
+                
+                <Navbar.Brand className="nav-bar-logo">
                     <img
-                    alt=" "
-                    src='./ectclogo.svg'
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
+                        alt=" "
+                        src='./ectclogo.svg'
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
                     />{' '}
                     ECTC2021
                 </Navbar.Brand>
+                <Navbar.Brand className="date">
+                    February 19 - 21
+                </Navbar.Brand>
+                <Navbar.Text className="register">
+                    Register
+                </Navbar.Text>
             </Navbar>
         )
     }
